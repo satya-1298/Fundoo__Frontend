39be0efe0b293d93b0c375e1751d8de9e0ac80e0
satya-1298 <satyaadabala1998@gmail.com>
2023-10-08 11:10:33 +0530

    [Satya] Refactor : Refactor the code after fixing bugs frontend fundoo

--- src/Component/Dashboard.jsx
+++ src/Component/Dashboard.jsx
@@ -12,59 +12,69 @@ export default function Dashboard() {
 
     const [showNoteTwo, setShowNoteTwo] = useState(true);
 
-   
+
     const [getnote, setGetnote] = useState([]);
 
-    const [section,setSection]=useState("notes")
+    const [collection, setCollection] = useState("notes")
 
     const handleToggle = () => {
 
         setShowNoteTwo(!showNoteTwo)
     }
 
-  
-    const AllNote = async() =>{
-        let response = await GetAllNote();
-        console.log(response.data.data)
-        let newArray = [];
-      
-        if(section === "notes"){
-          newArray = response.data.data.filter(filterData => filterData.isArchive === false && filterData.isTrash === false);
-          console.log('Note')
-        }
-      
-        else if(section === "archive"){
-          newArray = response.data.data.filter(filterData => filterData.isArchive === true && filterData.isTrash === false);
-        }
-      
-        else if(section === "trash"){
-          newArray = response.data.data.filter(filterData => filterData.isArchive === false && filterData.isTrash === true);
-        }
-        // SetGetAll(response.data.result) //'result' is data comming from Http response and we storing result data in "data"
-        console.log(newArray);
-        setGetnote(newArray);
-      };
 
-      useEffect(() => {
-            console.log("Data called")
-            AllNote()
-        },[getnote]);
-     
-    
+    const AllNote = async () => {
+
+        try {
+            let response = await GetAllNote();
+            console.log(response.data.data);
+
+            if (!response.data.data || response.data.data.length === 0) {
+                // Handle the case when there are no notes to display
+                console.log("No notes found.");
+                setGetnote([]); // Clear the existing notes
+            } else {
+                let newArray = [];
+
+                if (collection === "notes") {
+                    newArray = response.data.data.filter(filterData => filterData.isArchive === false && filterData.isTrash === false);
+                    console.log('Note');
+                } else if (collection === "archive") {
+                    newArray = response.data.data.filter(filterData => filterData.isArchive === true && filterData.isTrash === false);
+                } else if (collection === "trash") {
+                    newArray = response.data.data.filter(filterData => filterData.isArchive === false && filterData.isTrash === true);
+                }
+                console.log(newArray);
+                setGetnote(newArray);
+            }
+        } catch (error) {
+            console.error("Error in GetAllNote:", error);
+            throw error; 
+
+        };
+
+    };
+
+    useEffect(() => {
+        console.log("Data called")
+        AllNote()
+    }, [getnote]);
+
+
 
     return (
         <div>
-            <MiniDrawer setSection={setSection} />
+            <MiniDrawer setCollection={setCollection} />
             <div>
-            {
-                showNoteTwo ? <NoteOne handleToggle={handleToggle} /> : <NoteTwo handleToggle={handleToggle} AllNote={AllNote} />
-            }
+                {
+                    showNoteTwo ? <NoteOne handleToggle={handleToggle} /> : <NoteTwo handleToggle={handleToggle} AllNote={AllNote} />
+                }
             </div>
-           
+
             <div className="notethree">
                 {
                     getnote.map((data) =>
-                        (<NoteThree key={data.id} data={data} AllNote={AllNote} TrashSection={data.isTrash}/>))
+                        (<NoteThree key={data.id} data={data} AllNote={AllNote} TrashSection={data.isTrash} />))
                 }
             </div>
             {/* <NoteThree/> */}
--- src/WebPage/SignUp/SignUp.jsx
+++ src/WebPage/SignUp/SignUp.jsx
@@ -1,113 +1,109 @@
 
 
-import React,{useState} from "react";
+import React, { useState } from "react";
 import './SignUp.css';
 import Logo from "../../Assets/Logo.jpg"
-import {Link, useNavigate} from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { signup } from "../../Services/UserServices";
 // import SignIn from "./SignIn/SignIn";   
-import {TextField,Button} from '@mui/material';
-const RegFirstName =/^([A-Z]{1}[a-z,A-Z]{2,})$/;
-const RegLastName=/^([A-Z]{1}[a-z,A-Z]{2,})$/;
-const RegEmail=/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
-const RegPassword=/^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&-+=()])([a-zA-Z0-9]*).{8,}$/;
+import { TextField, Button } from '@mui/material';
+const RegFirstName = /^([A-Z]{1}[a-z,A-Z]{2,})$/;
+const RegLastName = /^([A-Z]{1}[a-z,A-Z]{2,})$/;
+const RegEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
+const RegPassword = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&-+=()])([a-zA-Z0-9]*).{8,}$/;
 
 
 
-export default function SignUp(){
+export default function SignUp() {
     // useState-hook to manage the component's state, 
-    const[data,setData]=useState({firstName:'',lastName:'',email:'',password:''});
-    const[regobj,setRegobj]=useState({firstNameBoarder:false,firstNameHelper:'',
-                                lastNameBoarder:false,lastNameHelper:'',
-                                emailBoarder:false, emailHelper:'', passwordBoarder:false,password_Helper:'' });
-  
-const navigate = useNavigate()
+    const [data, setData] = useState({ firstName: '', lastName: '', email: '', password: '' });
+    const [regobj, setRegobj] = useState({
+        firstNameBoarder: false, firstNameHelper: '',
+        lastNameBoarder: false, lastNameHelper: '',
+        emailBoarder: false, emailHelper: '', passwordBoarder: false, password_Helper: ''
+    });
 
-                const FirstName=(e)=>{
+    const navigate = useNavigate()
+
+    const FirstName = (e) => {
         setData(prevState => (
             {
                 ...prevState,
-                firstName:e.target.value
+                firstName: e.target.value
             }
         ))
         //console.log(event)
     }
-    const LastName=(e)=>{
-        setData(prevState =>(
+    const LastName = (e) => {
+        setData(prevState => (
             {
                 ...prevState,
-                lastName:e.target.value
+                lastName: e.target.value
             }
         ))
     }
-    const Email=(e)=>{
-        setData(prevState=>(
+    const Email = (e) => {
+        setData(prevState => (
             {
                 ...prevState,
-                email:e.target.value
+                email: e.target.value
             }
         ))
     }
-    const Password=(e)=>{
-        setData(prevState=>(
+    const Password = (e) => {
+        setData(prevState => (
             {
                 ...prevState,
-                password:e.target.value
+                password: e.target.value
             }
         ))
     }
-  
+
     console.log(data);
-   const verifyValidation=()=>
-   {
-        let validFirstName=RegFirstName.test(data.firstName)
-        if(validFirstName===false)
-        {
-            setRegobj(prevState=>(
-            {
-                    ...prevState,
-                    firstNameBoarder:true,
-                    firstNameHelper:'Enter Valid Information'
-            }))
-        }
-        let validateLastName=RegLastName.test(data.lastName)
-        if(validateLastName===false)
-        {
-            setRegobj(prevState=>(
+    const verifyValidation = () => {
+        let validFirstName = RegFirstName.test(data.firstName)
+        if (validFirstName === false) {
+            setRegobj(prevState => (
                 {
                     ...prevState,
-                    lastNameBoarder:true,
-                    lastNameHelper:'Enter Valid Information'
+                    firstNameBoarder: true,
+                    firstNameHelper: 'Enter Valid Information'
+                }))
+        }
+        let validateLastName = RegLastName.test(data.lastName)
+        if (validateLastName === false) {
+            setRegobj(prevState => (
+                {
+                    ...prevState,
+                    lastNameBoarder: true,
+                    lastNameHelper: 'Enter Valid Information'
                 }
             ))
         }
-        let validEmail=RegEmail.test(data.email)
-        if(validEmail===false)
-        {
-            setRegobj(prevState=>(
+        let validEmail = RegEmail.test(data.email)
+        if (validEmail === false) {
+            setRegobj(prevState => (
                 {
                     ...prevState,
-                    emailBoarder:true,
-                    emailHelper:'InValid Email'
+                    emailBoarder: true,
+                    emailHelper: 'InValid Email'
                 }
             ))
 
         }
-        let validatePassword=RegPassword.test(data.password)
-        if(validatePassword===false)
-        {
-            setRegobj(prevState=>(
+        let validatePassword = RegPassword.test(data.password)
+        if (validatePassword === false) {
+            setRegobj(prevState => (
                 {
                     ...prevState,
-                    passwordBoarder:true,
-                    password_Helper:'invalid'
+                    passwordBoarder: true,
+                    password_Helper: 'invalid'
                 }
             ))
         }
-        
-        
-        if(validFirstName==true && validateLastName==true && validEmail==true && validatePassword==true )
-        {
+
+
+        if (validFirstName == true && validateLastName == true && validEmail == true && validatePassword == true) {
             signup(data).then((response) => {
                 console.log(response)
                 navigate("/")
@@ -115,12 +111,12 @@ const navigate = useNavigate()
                 console.log(error)
             })
         }
-   }
+    }
 
-    return(
+    return (
         <div className="signUp-Containerr">
             <div className="main-containerr">
-               
+
                 <form>
                     <div className="form-containerr">
                         <div className="headerr">
@@ -131,47 +127,47 @@ const navigate = useNavigate()
                             <p className="o"><b>O</b></p>
                             <p className="oo"><b>O</b></p>
 
-                              
+
                         </div>
                         <div>
-                        <p className="heading-ss" >Create your Fundoo Account </p>    
+                            <p className="heading-ss" >Create your Fundoo Account </p>
                         </div>
                         <div className="name-ss">
-                            <TextField id="FirstName" label="FirstName" onChange={FirstName} size="small" error={regobj.firstNameBoarder} helperText={regobj.firstNameHelper} variant="outlined" className="input-ss"/>
-                         
-                            <TextField id="LastName" label="LastName" onChange={LastName} error={regobj.lastNameBoarder} helperText={regobj.lastNameHelper} variant="outlined" size="small" className="input-ss"/>
+                            <TextField id="FirstName" label="FirstName" onChange={FirstName} size="small" error={regobj.firstNameBoarder} helperText={regobj.firstNameHelper} variant="outlined" className="input-ss" />
+
+                            <TextField id="LastName" label="LastName" onChange={LastName} error={regobj.lastNameBoarder} helperText={regobj.lastNameHelper} variant="outlined" size="small" className="input-ss" />
                         </div>
-                        
-                            <div className="Username-SS">
-                            <TextField  id="Email" label="UserName" onChange={Email} error={regobj.emailBoarder} helperText={regobj.emailHelper}  variant="outlined" size="small"  className="input-sss"/>
-                              
-                            </div>
-                            
-                           <label className="label-ss">you can use letters,numbers & periods</label>
-                           <p className="para-ss">Use my current email instead</p>                            
+
+                        <div className="Username-SS">
+                            <TextField id="Email" label="UserName" onChange={Email} error={regobj.emailBoarder} helperText={regobj.emailHelper} variant="outlined" size="small" className="input-sss" />
+
+                        </div>
+
+                        <label className="label-ss">you can use letters,numbers & periods</label>
+                        <p className="para-ss">Use my current email instead</p>
                         <div className="pass-stylee" >
-                            <TextField type="password" id="Password" label="Password" onChange={Password}  variant="outlined" error={regobj.passwordBoarder} helperText={regobj.password_Helper} size="small"  className="input-ss"/>
-                            <TextField type="password" id="PasswordConfirm" label="Confirm" variant="outlined" size="small"  className="input-ss"/>
-                         
-                         </div>
-                         <p className="para-ss">Use 8 or more characters with a mix of letters,numbers & symbol</p>
-                         <div className="para-ss">
-                            <label> <input type="checkbox"/>Show Password
+                            <TextField type="password" id="Password" label="Password" onChange={Password} variant="outlined" error={regobj.passwordBoarder} helperText={regobj.password_Helper} size="small" className="input-ss" />
+                            <TextField type="password" id="PasswordConfirm" label="Confirm" variant="outlined" size="small" className="input-ss" />
+
+                        </div>
+                        <p className="para-ss">Use 8 or more characters with a mix of letters,numbers & symbol</p>
+                        <div className="para-ss">
+                            <label> <input type="checkbox" />Show Password
                             </label>
-                                
+
                         </div>
                         <div className="submitt">
-                            <Link to ="/login">
-                            <a href=''>sign in insted</a>
+                            <Link to="/login">
+                                <a href=''>sign in insted</a>
                             </Link>
                             {/* <a href="/login">
                              <Button onClick={verifyValidation} variant="contained" className="button-ss" >Next</Button>
                              </a> */}
-                              <Button onClick={verifyValidation} variant="contained" className="button-ss" >Next</Button>
+                            <Button onClick={verifyValidation} variant="contained" className="button-ss" >Next</Button>
                         </div>
                     </div>
                 </form>
-                
+
                 <div className="image-ss" >
                     <img src={Logo} />
                 </div>
